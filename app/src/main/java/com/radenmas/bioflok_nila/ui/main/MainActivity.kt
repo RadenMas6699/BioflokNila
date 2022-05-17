489101d58e8adc82ce624fc1fb645eaa95ca9187
RadenMas6699 <radenmas.dev@gmail.com>
2022-05-17 15:21:30 +0800

    update

--- app/src/main/java/com/radenmas/bioflok_nila/ui/main/MainActivity.kt
+++ app/src/main/java/com/radenmas/bioflok_nila/ui/main/MainActivity.kt
@@ -5,13 +5,30 @@
 
 package com.radenmas.bioflok_nila.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.radenmas.bioflok_nila.R
+import com.radenmas.bioflok_nila.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var b: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+        b = ActivityMainBinding.inflate(layoutInflater)
+        val v = b.root
+        setContentView(v)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        b.navBottom.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/radenmas/bioflok_nila/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/radenmas/bioflok_nila/ui/splash/SplashActivity.kt
@@ -5,13 +5,27 @@
 
 package com.radenmas.bioflok_nila.ui.splash
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.radenmas.bioflok_nila.R
+import com.radenmas.bioflok_nila.ui.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1500)
     }
 }
